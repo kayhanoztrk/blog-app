1f2c8ae8acc47b88ce9d300d5b8a0f59104e149b
Kayhan <dev.kayhanozturk@gmail.com>
2023-01-08 13:25:43 +0300

    cardComponent has been added for blog posts

---
+++ blog-frontend/src/components/Card/Card.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: 800,
+      textAlign : "left",
+      margin : 20
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    avatar: {
+      background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    },
+    link: {
+        textDecoration : "none",
+        boxShadow : "none",
+        color : "white"
+    }
+  }));
+
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const classes = useStyles();
+  
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant='h3'  gutterBottom>
+          A Simple MUI Card
+        </Typography>
+        <Typography variant="h4" component="div">
+         Heading
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          describes the heading
+        </Typography>
+        <Typography variant="body1">
+          Card content
+          <br />
+          {'"describes the content"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Card Button</Button>
+      </CardActions>
+    </Card>
+    </div>
+  );
+}
--- blog-frontend/src/components/Home/Home.js
+++ blog-frontend/src/components/Home/Home.js
@@ -1,10 +1,34 @@
 import React, {useState, useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import RecipeReviewCard from "../Card/Card";
+
+const useStyles = makeStyles((theme) => ({
+    container:{
+        display :"flex",
+        flexWrap :"wrap",
+        justifyContent : "center",
+        alignItems: "center",
+        backgroundColor: "#f0f5ff"
+
+    }
+}));
 
 const Home = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [postList, setPostList] = useState([]);
 
-    return(<>
-    <h3>HomePage!</h3></>
-    );
+
+
+    const classes = useStyles();
+    return(
+        <div className={classes.container}>
+            <RecipeReviewCard />
+            <RecipeReviewCard />
+            <RecipeReviewCard />
+
+        </div>
+
+    )
 }
 
 export default Home;
